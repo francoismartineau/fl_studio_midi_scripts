6cbd26400dc603cc629a5d464333e393d7e42765
Francois Martineau <ffrancoismmartineau@gmail.com>
2021-04-09 22:02:39 -0400

    Device naming corrections

--- 1/device_1.py
+++ 1/device_1.py
@@ -1,33 +1,81 @@
 # name=1
 """
     This midi script allows for communications between FLahk and FL Studio using midi messages.
-    device_1 does unidirectionnal communication (FLahk to FL Studio)
+    device_1 does unidirectional communication (FLahk to FL Studio)
     device_2 does bidirectional communication. FLahk asks questions to FL Studio, which answers back
 """
 import device, midi, mixer, patterns, transport, channels
 import time
 
 global transport_flush_time
+transport_flush_time = time.time()
 
 # ----------------------------------------------------------
-def test_FL(_):
-    print("test_FL")
-    return 49
+def set_pattern(n):
+    if type(n) is str and n.isdigit():
+        n = int(n)
+    if type(n) is int:
+        patterns.jumpToPattern(n)
+        print("set_pattern:", n)
 
-def get_pattern(_):
-    p = patterns.patternNumber()
-    print("get_pattern:", p)
-    return p
+def set_mixer_track_route(n):
+    if type(n) is str and n.isdigit():
+        n = int(n)
+    currTrack = mixer.trackNumber()
+    for dest in range(mixer.trackCount()):
+        active = mixer.getRouteSendActive(currTrack, dest)
+        if active:
+            mixer.setRouteTo(currTrack, dest, 0)
+    mixer.setRouteTo(currTrack, n, 1)
+    mixer.getRouteSendActive(currTrack, n)
+    print("set_mixer_track_route:", n)
 
-def get_mixer_track(_):
-    n = mixer.trackNumber()
-    print("get_mixer_track:", n)
-    return n  
+def set_mixer_track(n):
+    mixer.deselectAll()
+    mixer.setTrackNumber(n)
+    print("set_mixer_track:", n)
+
+def toggle_play_pause_twice(_):
+    toggle_play_pause(_)
+    toggle_play_pause(_)
+
+def stop(_):
+    print("stop")
+    transport.globalTransport(midi.FPT_Stop, 1)
+    set_transport_flush_time()
+
+def toggle_rec(_):
+    print("toggle_rec")
+    transport.globalTransport(midi.FPT_Record, 1)  
+
+def toggle_play_pause(_):
+    print("toggle_play_pause")
+    transport.globalTransport(midi.FPT_Play, 1)
+    set_transport_flush_time()
+
+saved_song_pos = 0
+def save_load_song_pos(save_load):
+    global saved_song_pos
+    if save_load == 1:
+        print("save_song_pos")
+        saved_song_pos = transport.getSongPos(midi.SONGLENGTH_ABSTICKS)
+    elif save_load == 0:
+        print("load_song_pos")
+        transport.setSongPos(saved_song_pos, midi.SONGLENGTH_ABSTICKS)
+
+def deselect_all_channels(_):
+    channels.deselectAll()
 
 functions = {
-    50 : test_FL,
-    127 : get_pattern,
-    124 : get_mixer_track,
+    126 : set_pattern,
+    125 : set_mixer_track_route,
+    123 : set_mixer_track,
+    122 : toggle_play_pause_twice,
+    121 : stop,
+    120 : toggle_rec,
+    119 : toggle_play_pause,
+    118 : save_load_song_pos,
+    117 : deselect_all_channels
 }
 
 # ----------------------------------------------------------
@@ -41,34 +89,27 @@ def OnMidiMsg(event):
             func = event.controlNum
             param = event.controlVal
             print("----------------------------")
-            answer = functions[func](param)
-            send(answer)
-        #print_event(event)
+            functions[func](param)
+        elif chan == 16 or (chan < 15 and time_since_transport_flush() < .1):
+            event.handled = True
+        print_event(event)
 
+# ------------------------------------------------------
+def set_transport_flush_time():
+    global transport_flush_time
+    transport_flush_time = time.time()
 
-
-# ---------------------------------------------
-def is_numpad(event):
-    return event.controlNum == 100
-
-def send(answer):
-    # 0xB0: it's a cc messave
-    # 0x0F: sent on chan 16
-    # 127 : cc 127
-    sendMidiBytes(0xBF, 127, answer)
-
-def sendMidiBytes(byte1, byte2, byte3):
-    msg = byte1 + (byte2 << 8) + (byte3 << 16)
-    device.midiOutMsg(msg)
+def time_since_transport_flush():
+    global transport_flush_time
+    return time.time() - transport_flush_time
 
 def print_event(event):
-    event.handled
     chan = event.midiChan + 1
     cc = event.controlNum
     val = event.controlVal
     handled = ""
     if event.handled:
-        handled = "x"
+        handled = "already handled"
     msg = "{}  chan: {}    cc: {}    val: {}".format(handled, chan, cc, val)
     print(msg)
 
